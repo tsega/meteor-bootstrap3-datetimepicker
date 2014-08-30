5c77edae4afc350378377a6b30a99be03865fad5
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2014-08-30 10:19:49 +0300

    Update package for use with new Meteor Packaging system.

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 Package.describe({
-    summary: "Bootstrap 3 DateTime picker from Eonasdan, https://github.com/Eonasdan/bootstrap-datetimepicker, packaged for Meteor.js"
+    summary: "Bootstrap 3 DateTime picker from Eonasdan, https://github.com/Eonasdan/bootstrap-datetimepicker, packaged for Meteor.js",
+    version: "0.4.0",
+    git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker.git"
 });
 
-Package.on_use(function (api) {
+Package.on_use(function (api, where) {
+    api.versionsFrom('METEOR@0.9.0');
+
     api.use('jquery', 'client');
 
-    // Add Moment with languages directly
-    api.add_files('lib/js/moment-with-locales.min.js', 'client');
-
-    // Add Date Time Picker CSS and JS files
-    api.add_files('lib/css/bootstrap3-datetimepicker.min.css', 'client');
-    api.add_files('lib/js/bootstrap3-datetimepicker.min.js', 'client');
+    api.add_files([
+      'lib/js/moment-with-locales.min.js', // Add Moment with languages directly
+      'lib/css/bootstrap3-datetimepicker.min.css', // Add Date Time Picker CSS and JS files
+      'lib/js/bootstrap3-datetimepicker.min.js',
+    ], 'client');
 });
