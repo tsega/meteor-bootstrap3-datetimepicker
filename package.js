676691066d1e6b5e6c5a5fc2a5c5cf934a96f9ae
emgee3 <hello@gravitronic.com>
2014-09-26 02:14:18 -0700

    remove bundled copy of moments

--- package.js
+++ package.js
@@ -8,9 +8,9 @@ Package.on_use(function (api, where) {
     api.versionsFrom('METEOR@0.9.0');
 
     api.use('jquery', 'client');
+    api.use('mrt:moment@2.8.1', 'client');
 
     api.add_files([
-      'lib/js/moment-with-locales.min.js', // Add Moment with languages directly
       'lib/css/bootstrap3-datetimepicker.css', // Add Date Time Picker CSS and JS files
       'lib/js/bootstrap3-datetimepicker.js',
     ], 'client');
