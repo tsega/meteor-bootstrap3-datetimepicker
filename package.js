470882e129f7691b177b899e2f88f59a304099ea
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2014-12-25 14:05:09 +0300

    Added moment package use on the server. Bumped version to 3.1.3_3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
-    version: "3.1.3_2",
+    version: "3.1.3_3",
     git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker.git"
 });
 
@@ -8,7 +8,7 @@ Package.on_use(function (api, where) {
     api.versionsFrom('METEOR@0.9.0');
 
     api.use('jquery', 'client');
-    api.use('momentjs:moment@2.8.4', 'client');
+    api.use('momentjs:moment@2.8.4');
 
     api.add_files([
       'lib/css/bootstrap3-datetimepicker.css', // Add Date Time Picker CSS and JS files
