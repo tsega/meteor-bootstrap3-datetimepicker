e07c7f2f8bc81c6d46425642bbb06a6568e09456
Reio Piller <reio@heathmont.net>
2015-03-16 15:49:14 +0200

    bump version to 4.7.14

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
-    version: "4.0.0_4",
+    version: "4.7.14",
     git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker.git"
 });
 
