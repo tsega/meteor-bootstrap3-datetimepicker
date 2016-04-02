400bca1d15a78a2b24554092f92de31fe0436ba5
ScyDev <lukas.saegesser@gmail.com>
2016-04-02 12:07:46 +0200

    - meteor 1.2 compatibility: added package name

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
-    summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
-    version: "4.17.37_2",
-    git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker"
+  name: 'tsega:bootstrap3-datetimepicker',
+  summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
+  version: "4.17.37_2",
+  git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker"
 });
 
 Package.onUse(function (api) {
