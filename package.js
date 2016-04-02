5c32cd147ecefd72e24e870523aceb4df3ad10ab
ScyDev <lukas.saegesser@gmail.com>
2016-04-02 11:28:53 +0200

    - made it work with meteor 1.2

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
     summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
-    version: "4.17.37_1",
+    version: "4.17.37_2",
     git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker"
 });
 
-Package.on_use(function (api, where) {
-    api.versionsFrom('METEOR@0.9.0');
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@1.2');
 
-    api.use('jquery', 'client');
+    api.use('jquery');
     api.use('momentjs:moment@2.8.4');
 
-    api.add_files([
+    api.addFiles([
       'lib/css/bootstrap-datetimepicker.css', // Add Date Time Picker CSS and JS files
       'lib/js/bootstrap-datetimepicker.js',
     ], 'client');
